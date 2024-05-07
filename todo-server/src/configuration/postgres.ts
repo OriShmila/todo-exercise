954f486b4a8bf862689fd9ef290a1894de0a836f
Ori-Shmila <64976670+OriShmila@users.noreply.github.com>
2024-05-07 16:40:00 +0300

    Update postgres.ts

--- todo-server/src/configuration/postgres.ts
+++ todo-server/src/configuration/postgres.ts
@@ -5,8 +5,7 @@ pg.defaults.ssl = {
 };
 
 export const POSTGRES_URI =
-  process.env.POSTGRES_DATABASE_URL ||
-  `postgres://hcyiowfmiindvk:ff3188920c4c5c6aa9ccf2d46c4fa937636f1b05d98308746e26d05f37b8ca23@ec2-54-158-222-248.compute-1.amazonaws.com:5432/d1l4p8g32nnv2g`;
+  process.env.POSTGRES_DATABASE_URL;
 
 export const connectPostgresDb = async (connectionString: string) => {
   try {
